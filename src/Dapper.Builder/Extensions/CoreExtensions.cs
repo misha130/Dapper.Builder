094fb322c6a3cd7a9cbe6f09879d3ea76d292caa
Misha Tarnortusky <michael@sensory.co.il>
2019-05-19 20:11:43 -0400

    init

---
+++ src/Dapper.Builder/Extensions/CoreExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dapper.Builder.Extensions
+{
+    public static class CoreExtensions
+    {
+        public static void AddDapperBuilder(this IServiceCollection services)
+        {
+
+        }
+    }
+}
